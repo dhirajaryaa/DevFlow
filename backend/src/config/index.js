b86189798275ac53a0be48272c40fa62ff3ba685
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-03-18 16:31:34 +0530

    feat: implement JWT verification middleware for user authentication

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -4,35 +4,35 @@ dotenv.config({
   path: "./.env",
 });
 
-const config = {
+const config = Object.freeze({
   port: process.env.PORT || 5000,
   mongodb_uri: process.env.MONGODB_URI,
   db_name: process.env.DB_NAME,
-};
+});
 
-const cloudinaryConfig = {
+const cloudinaryConfig = Object.freeze({
   cloudName: process.env.CLOUDINARY_CLOUD_NAME,
   apiKey: process.env.CLOUDINARY_API_KEY,
   apiSecret: process.env.CLOUDINARY_API_SECRET,
-};
+});
 
-const jwtConfig = {
+const jwtConfig = Object.freeze({
   accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
   accessTokenExpiry: process.env.ACCESS_TOKEN_EXPIRY,
   refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
   refreshTokenExpiry: process.env.REFRESH_TOKEN_EXPIRY,
-};
+});
 
-const corsConfig = {
+const corsConfig = Object.freeze({
   production: process.env.PRODUCTION === "true",
   methods: process.env.METHODS ? process.env.METHODS.split(",") : [],
   allowedUrl: process.env.ALLOWED_URL ? process.env.ALLOWED_URL.split(",") : [],
-};
+});
 
-const tokenOptions = {
+const tokenOptions = Object.freeze({
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: "strict",
-};
+});
 
 export { config, cloudinaryConfig, jwtConfig, corsConfig, tokenOptions };
---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+import { AsyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import { jwtConfig } from "../config/index.js";
+import { User } from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = AsyncHandler(async function (req, res, next) {
+  const incomingToken =
+    req.cookies.accessToken || req.headers.authorization.split("Bearer ")[0];
+  if (!incomingToken) {
+    throw new ApiError(400, "Invalid Credential");
+  }
+  const decodedToken = await jwt.verify(
+    incomingToken,
+    jwtConfig.accessTokenSecret
+  );
+  if (!decodedToken) {
+    throw new ApiError(401, "Unauthorized");
+  }
+  const user = await User.findById(decodedToken._id).select(
+    "-password -refreshToken"
+  );
+  req.user = user;
+  next();
+});
