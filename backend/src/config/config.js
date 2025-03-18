abe984f5fd42172a7981ec79df08800041e7137d
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-03-18 10:40:52 +0530

    Add configuration file for environment variables and service settings

---
+++ backend/src/config/config.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const config = {
+  port: process.env.PORT || 3000,
+  mongodb_uri: process.env.MONGODB_URI,
+  db_name: process.env.DB_NAME,
+};
+
+const cloudinaryConfig = {
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  apiKey: process.env.CLOUDINARY_API_KEY,
+  apiSecret: process.env.CLOUDINARY_API_SECRET,
+};
+
+const jwtConfig = {
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+  accessTokenExpiry: process.env.ACCESS_TOKEN_EXPIRY,
+  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
+  refreshTokenExpiry: process.env.REFRESH_TOKEN_EXPIRY,
+};
+
+const corsConfig = {
+  production: process.env.PRODUCTION === "true",
+  methods: process.env.METHODS.split(","),
+  allowedUrl: JSON.parse(process.env.ALLOWED_URL),
+};
+
+export  {
+  config,
+  cloudinaryConfig,
+  jwtConfig,
+  corsConfig,
+};
