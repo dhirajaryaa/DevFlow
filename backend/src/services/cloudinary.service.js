60166d649d72b523e17f5f16e6adb9ee17c02b63
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-03-18 17:30:00 +0530

    feat: add multer middleware for file uploads and cloudinary service for image storage

---
+++ backend/src/services/cloudinary.service.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import { cloudinaryConfig } from "../config/index.js";
+import { AsyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import fs from "fs";
+import { loadEnvFile } from "process";
+
+// configure cloudinary
+cloudinary.config({
+  cloud_name: cloudinaryConfig.cloudName,
+  api_key: cloudinaryConfig.apiKey,
+  api_secret: cloudinaryConfig.apiSecret,
+});
+
+// upload Avatar on Cloudinary
+const uploadOnCloudinary = AsyncHandler(async (localFilePath) => {
+  if (!localFilePath) {
+    throw new ApiError(400, "File don't Exist");
+  }
+  const res = await cloudinary.uploader.upload(localFilePath, {
+    resource_type: "auto",
+  });
+  await fs.unlinkSync(localFilePath);
+  return res;
+});
+
+export { uploadOnCloudinary };
