f8e2d9dbe824a12434ce1716921333e9cf1766eb
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-03-18 12:46:14 +0530

    feat: add password verification and token generation methods to user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { jwtConfig } from "../config/index.js";
 
 const userSchema = new mongoose.Schema(
   {
@@ -43,12 +44,40 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", async function(next) {
-    if (!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 12);
-    next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
+userSchema.methods.generateAccessToken = async function () {
+  return await jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      name: this.name,
+    },
+    jwtConfig.accessTokenSecret,
+    {
+      expiresIn: jwtConfig.accessTokenExpiry,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = async function () {
+  return await jwt.sign(
+    {
+      _id: this._id,
+    },
+    jwtConfig.refreshTokenSecret,
+    {
+      expiresIn: jwtConfig.refreshTokenExpiry,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
